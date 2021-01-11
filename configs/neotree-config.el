24920c870800f262a9535bba3f7a11b48a90c74b
Tran Anh Vu <eyelight93@gmail.com>
2021-01-11 21:51:28 +0700

    change neotree-toggle key binding

--- configs/neotree-config.el
+++ configs/neotree-config.el
@@ -3,7 +3,7 @@
   :ensure t
   :init
   (setq neo-theme (if (display-graphic-p) 'icons 'arrow)))
-(global-set-key [f8] 'neotree-toggle)
+(global-set-key "\M-8" 'neotree-toggle)
 (evil-define-key 'normal neotree-mode-map (kbd "RET") 'neotree-enter)
 
 (provide 'neotree-config)
