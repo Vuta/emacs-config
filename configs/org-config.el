cf4feca5bed45003cc5b14c96f98d4bf7d264424
Tran Anh Vu <eyelight93@gmail.com>
2021-04-11 09:50:54 +0700

    update config

--- configs/org-config.el
+++ configs/org-config.el
@@ -6,4 +6,5 @@
 (setq org-log-done 'time)
 (setq org-agenda-start-with-log-mode t)
 (setq org-log-into-drawer t)
+(setq org-todo-keywords '((sequence "TODO" "IN-PROGRESS" "DONE")))
 (provide 'org-config)
